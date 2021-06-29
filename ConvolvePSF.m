4f28ea8ec3ede5702f3d7705ab2c52713f72aae0
Ivan Kuznetsov <ikuz@Ivans-MBP.fios-router.home>
2021-06-29 02:03:09 -0400

    Organize

--- ConvolvePSF.m
+++
@@ -1,8 +0,0 @@
-function [c_intrp_blurred] = ConvolvePSF(data,PSF)
-    c_intrp_blurred = zeros(size(data),'single');
-    for i=1:size(data,4)%[1,size(desired_times,2)]%
-        i/size(data,4)
-        c_intrp_blurred(:,:,:,i) = convolution3D_FFTdomain(data(:,:,:,i),single(PSF));
-        %c_intrp_blurred(:,:,:,i) = convn(data(:,:,:,i),PSF),'same');
-    end
-end
