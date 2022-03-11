1c3e3e62e19e77e4fb0699e477d9e15eaabae719
Ivan Kuznetsov <ikuz@Ivans-MacBook-Pro.local>
2022-03-10 21:28:10 -0500

    Add

--- Examples/Example2/ZStack2Mesh.m
+++ Examples/Example2/ZStack2Mesh.m
@@ -1,5 +1,28 @@
-filename = 'Z-2-proc-alt-2.tif';
-param.im_file = 'Cell-2-slice.tif';
+%% Set parameters
+%Set parameter file here
+filename = './Examples/Example2/params';
+
+addpath ./Functions
+
+%Loads in parameters
+fid = fopen([filename,'.txt']);
+ims_and_rois = textscan(fid,'%s %s','delimiter',' ','MultipleDelimsAsOne',1,'CommentStyle','%');
+fclose(fid);
+param = cell2struct(num2cell(ims_and_rois{2}),ims_and_rois{1}');
+
+fn = fieldnames(param);
+for k=1:numel(fn)
+    if(isnan(str2double((param.(fn{k})))))
+        param.(fn{k}) = param.(fn{k}){1};
+    else
+        param.(fn{k}) = str2double(param.(fn{k}){1});
+    end
+end
+
+
+filename = './Examples/Example2/Z-stack.tif';
+param.threshold_cyto = 0.3;
+param.threshold_nuc = 0.35;
 tstack = Tiff(filename,'r');
 
 [i,j] = size(tstack.read());
@@ -35,40 +58,6 @@ cytoplasm = poly2mask(cytoplasm_contour(:,1),cytoplasm_contour(:,2),size(data,1)
 slice = idx_mid;
 I = data(:,:,idx_mid);
 
-% I = data(:,:,slice);
-% I = imsharpen(I);
-% [L,Centers] = imsegkmeans(uint8(I*255),4);
-% avg = [];
-% for i=1:4
-%     avg = [avg,mean(I(L==i))];
-% end
-% [~,idx] = min(avg);
-% 
-% L_back = L==idx;
-% L_front = ~L_back;
-% 
-% 
-% L(L_back) = 0;
-% L(L_front) = 1;
-% bw  = logical(L);
-% 
-% se = strel('disk',5);
-% bw = imclose(bw,se);
-% cytoplasm = bwareaopen(bw,100);
-% cytoplasm_and_nucleus = imfill(cytoplasm,'holes');
-% nucleus = cytoplasm_and_nucleus & ~cytoplasm;
-
-%imshow(I)
-%hold on
-%visboundaries(cytoplasm_and_nucleus,'Color','r');
-%visboundaries(nucleus,'Color','y');
-%set(gca,'units','pixels') % set the axes units to pixels
-%x = get(gca,'position') % get the position of the axes
-%set(gcf,'units','pixels') % set the figure units to pixels
-%y = get(gcf,'position') % get the figure position
-%set(gcf,'position',[y(1) y(2) x(3) x(4)])% set the position of the figure to the length and width of the axes
-%set(gca,'units','normalized','position',[0 0 1 1]) % set the axes units to pixels
-
 cytoplasm_and_nucleus = cytoplasm | nucleus;
 
 cytoplasm_store = cytoplasm_and_nucleus;
@@ -81,14 +70,13 @@ idx = find(avgs > 0.1*(mean(avgs)));
 idx = min(idx):max(idx);
 
 slice=127;
-idx=80:170;
+idx=80:210;
 %%
+addpath ./Dependencies/image3
 clf
 T = [1 0 0 0;0 1 0 0;0 0 1.5 0];
 for i=idx(10:20:end-10)
-    h1 = slice3(uint8(1.5*255*data),T,3,i);
-    %  h2 = slice3(squeeze(D),T,2,64);
-    %  h3 = slice3(squeeze(D),T,3,14);
+    h1 = slice3(uint8(15*1/mean(data(:))*data),T,3,i);
     set(h1,'EdgeColor','white','LineStyle','-');
 end
 colormap gray(88);
@@ -107,10 +95,9 @@ for_v = foreground(foreground>0);
 for i = fliplr(idx(1):slice)
     count = count+1;
     I = data(:,:,i);
-    I(I > quantile(for_v,0.02)) = 1;
-    I(I <= quantile(for_v,0.02)) = 0;
+    I(I > quantile(for_v,param.threshold_cyto)) = 1;
+    I(I <= quantile(for_v,param.threshold_cyto)) = 0;
     se = strel('disk',2);
-    %I = imclose(I,se);
     bw = activecontour(I,cytoplasm_and_nucleus,500,'Chan-Vese','ContractionBias',.1);
     cytoplasm_and_nucleus = bwareaopen(imfill(imclose(bw,se),'holes'),100);
     cyt_3D(:,:,slice-idx(1)-count+2) = cytoplasm_and_nucleus;
@@ -123,9 +110,8 @@ for i = fliplr(idx(1):slice)
     c = floor(mean(c));
     
     I = data(:,:,i);
-    I(I > quantile(for_v,0.01)) = 1;
-    I(I <= quantile(for_v,0.01)) = 0;
-    %imagesc(I)
+    I(I > quantile(for_v,param.threshold_nuc)) = 1;
+    I(I <= quantile(for_v,param.threshold_nuc)) = 0;
     mask = zeros(size(nucleus));
     y = 1:size(nucleus,1);
     x = 1:size(nucleus,2);
@@ -143,12 +129,9 @@ for i = fliplr(idx(1):slice)
     end
 
 
-    %nucleus = nucleus_filled - nucleus;
-    %nucleus = bw;
     if mean(I(nucleus_only)) > 0.5
         nucleus_only = zeros(size(nucleus_only));
     end
-    %cyt_3D(:,:,count) = bwareaopen(bw,100);
     nuc_3D(:,:,slice-idx(1)-count+2) = nucleus_only;
     nucleus = nucleus_only;
     visboundaries(nucleus_only,'Color','g');
@@ -159,10 +142,9 @@ nucleus = nucleus_store;
 for i = (slice+1):idx(end)
     count = count+1;
     I = data(:,:,i);
-    I(I > quantile(for_v,0.02)) = 1;
-    I(I <= quantile(for_v,0.02)) = 0;
-    %se = strel('disk',2);
-    %I = imclose(I,se);
+    I(I > quantile(for_v,param.threshold_cyto)) = 1;
+    I(I <= quantile(for_v,param.threshold_cyto)) = 0;
+
     bw = activecontour(I,cytoplasm_and_nucleus,500,'Chan-Vese','ContractionBias',.1);
     cytoplasm_and_nucleus = bwareaopen(imfill(imclose(bw,se),'holes'),100);
     cyt_3D(:,:,count) = cytoplasm_and_nucleus;
@@ -175,16 +157,15 @@ for i = (slice+1):idx(end)
     c = floor(mean(c));
     
     I = data(:,:,i);
-    I(I > quantile(for_v,0.01)) = 1;
-    I(I <= quantile(for_v,0.01)) = 0;
-    %imagesc(I)
+    I(I > quantile(for_v,param.threshold_nuc)) = 1;
+    I(I <= quantile(for_v,param.threshold_nuc)) = 0;
 
     y = 1:size(nucleus,1);
     x = 1:size(nucleus,2);
     [X,Y] = meshgrid(x,y);
     nucleus = (X-c).^2 + (Y-r).^2 < 25;
     visboundaries(nucleus,'Color','k');
-    bw = activecontour(I,nucleus,300,'Chan-Vese','SmoothFactor',0,'ContractionBias',-0.1);
+    bw = activecontour(I,nucleus,300,'Chan-Vese','SmoothFactor',0,'ContractionBias',0);
     se = strel('disk',3);
     nucleus = imclose(bw,se);
     nucleus_filled = imfill(nucleus,'holes');
@@ -193,12 +174,10 @@ for i = (slice+1):idx(end)
     else
         nucleus_only = nucleus_filled;
     end
-    %nucleus = nucleus_filled - nucleus;
-    %nucleus = bw;
+
     if mean(I(nucleus_only)) > 0.5
         nucleus_only = zeros(size(nucleus_only));
     end
-    %cyt_3D(:,:,count) = bwareaopen(bw,100);
     nuc_3D(:,:,count) = nucleus_only;
     nucleus = nucleus_only;
     visboundaries(nucleus_only,'Color','g');
